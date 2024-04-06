3b81ed24b8cbadbd5d162fd886778c841505e818
Tamzid79 <98099305+Tamzid79@users.noreply.github.com>
2024-04-06 19:47:31 +0600

    Final tweaks

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,120 +1,81 @@
-@extends('layouts.userApp')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Student Register</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+    <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
+</head>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+<body>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+    <main class="d-flex w-100">
+        <div class="container d-flex flex-column">
+            <div class="row vh-100">
+                <div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto d-table h-100">
+                    <div class="d-table-cell align-middle">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        
-                        <div class="form-group{{ $errors->has('institute') ? ' has-error' : '' }}">
-                            <label for="institute" class="col-md-4 control-label">Institute</label>
-
-                            <div class="col-md-6">
-                                <!--<input id="institute" type="text" class="form-control" name="institute" value="{{ old('institute') }}" required autofocus>-->
-                                <select name="institute" id="institute" class="form-control" required autofocus>
-                                    <option value="">--Select an Institute--</option>
-                                    <option value="None">None @if(old('institute') == "None"){{'selected'}}@endif</option>
-                                    <option value="NIBM">NIBM @if(old('institute') == "NIBM"){{'selected'}}@endif</option>
-                                    <option value="UCSC">UCSC @if(old('institute') == "UCSC"){{'selected'}}@endif</option>
-                                    <option value="SLIIT">SLIIT @if(old('institute') == "SLIIT"){{'selected'}}@endif</option>
-                                    <option value="ESOFT">ESOFT @if(old('institute') == "ESOFT"){{'selected'}}@endif</option>
-                                    <option value="IIT">IIT @if(old('institute') == "IIT"){{'selected'}}@endif</option>
-                                    <option value="ICBT">ICBT @if(old('institute') == "ICBT"){{'selected'}}@endif</option>                                    
-                                </select>
-                                @if ($errors->has('institute'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('institute') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        
-                        <div class="form-group{{ $errors->has('gradYear') ? ' has-error' : '' }}">
-                            <label for="gradYear" class="col-md-4 control-label">Paper Year</label>
-
-                            <div class="col-md-6">
-                                <!--<input id="institute" type="text" class="form-control" name="institute" value="{{ old('institute') }}" required autofocus>-->
-                                <select name="gradYear" id="gradYear" class="form-control" required autofocus>
-                                    <option value="">--Select Your Ungrad Year--</option>
-                                    <option value="1">1 @if(old('gradYear') == "1"){{'selected'}}@endif</option>                                   
-                                    <option value="2">2 @if(old('gradYear') == "2"){{'selected'}}@endif</option>
-                                    <option value="3">3 @if(old('gradYear') == "3"){{'selected'}}@endif</option>
-                                    <option value="4">4 @if(old('gradYear') == "4"){{'selected'}}@endif</option>                                    
-                                </select>
-                                @if ($errors->has('gradYear'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('gradYear') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="text-center mt-4">
+                            <h1 class="h2">Get started</h1>
+                            <p class="lead">
+                                Create a new account to continue
+                            </p>
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="m-sm-3">
+                                    <form action="{{ route('register') }}" method="POST">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label class="form-label">Name</label>
+                                            <input class="form-control form-control-lg" type="text" name="name"
+                                                placeholder="Enter your name" required />
+                                            @error('name')
+                                                <p class="text-danger">{{ $message }}</p>
+                                            @enderror
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Email</label>
+                                            <input class="form-control form-control-lg" type="email" name="email"
+                                                placeholder="Enter your email" required />
+                                            @error('email')
+                                                <p class="text-danger">{{ $message }}</p>
+                                            @enderror
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Password</label>
+                                            <input class="form-control form-control-lg" type="password" name="password"
+                                                placeholder="Enter password" required />
+                                            @error('password')
+                                                <p class="text-danger">{{ $message }}</p>
+                                            @enderror
+                                        </div>
+                                        <div class="mb-3">
+                                            <label class="form-label">Confirm Password</label>
+                                            <input class="form-control form-control-lg" type="password"
+                                                name="password_confirmation" placeholder="Retype password" required />
+                                        </div>
+                                        <div class="d-grid gap-2 mt-3">
+                                            <button type="submit" class="btn btn-lg btn-primary">Sign up</button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="text-center mb-3">
+                            Already have account? <a href="{{ route('login') }}">Log In</a>
                         </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </main>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,68 @@
 <?php
 
+use App\Http\Controllers\admin\categoryController as AdminCategoryController;
+use App\Http\Controllers\admin\courseController;
+use App\Http\Controllers\admin\examController as AdminExamController;
+use App\Http\Controllers\admin\questionController as AdminQuestionController;
+use App\Http\Controllers\admin\studentController as AdminStudentController;
+use App\Http\Controllers\admin\userController;
+use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\studentExamController;
+use App\Http\Controllers\teacher\categoryController;
+use App\Http\Controllers\teacher\examController;
+use App\Http\Controllers\teacher\questionController;
+use App\Http\Controllers\teacher\studentController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'PagesController@index');
-Route::get('/about', 'PagesController@about');
-Route::get('/Contributors', 'PagesController@institutesInfo');
+Route::group(['prefix' => 'admin', 'as' => 'admin.','middleware' => ['auth', 'admin']],function(){
+    Route::get('dashboard', [dashboardController::class, 'adminDashboard'])->name('dashboard');
+    Route::resource('courses', courseController::class);
+    Route::resource('categories', AdminCategoryController::class);
+    Route::resource('questions', AdminQuestionController::class);
+    Route::resource('exams', AdminExamController::class);
+    
+    Route::get('students', [AdminStudentController::class, 'index'])->name('students.index');
+    Route::get('students/{id}', [AdminStudentController::class, 'show'])->name('students.show');
+    Route::get('students/{sId}/details/{examId}', [AdminStudentController::class, 'showDetails'])->name('students.show.details');
+    
+    Route::resource('users', userController::class);
+});
 
-Route::resource('exampapers', 'ExamPapersController');
-Route::resource('questions', 'QuestionsController');
-Route::resource('uanswer', 'StudentAnswersController');
-Route::resource('result', 'StudentResultController');
+Route::group(['prefix' => 'teacher', 'as' => 'teacher.','middleware' => ['auth', 'teacher']],function(){
+    Route::get('dashboard', [dashboardController::class, 'teacherDashboard'])->name('dashboard');
+    Route::resource('categories', categoryController::class);
+    Route::resource('questions', questionController::class);
+    Route::resource('exams', examController::class);
+
+    Route::get('students', [studentController::class, 'index'])->name('students.index');
+    Route::get('students/{id}', [studentController::class, 'show'])->name('students.show');
+    Route::get('students/{sId}/details/{examId}', [studentController::class, 'showDetails'])->name('students.show.details');
+});
+
+Route::group(['middleware' => ['auth', 'student']],function(){
+
+    Route::get('/', [dashboardController::class, 'dashboard']);
+    Route::get('course/{id}', [dashboardController::class, 'singleCourse'])->name('singleCourse');
+    Route::get('my-results', [dashboardController::class, 'myResults'])->name('myResults');
+
+    Route::get('exam/{id}', [studentExamController::class, 'liveExam'])->name('liveExam');
+    Route::post('exam/submit', [studentExamController::class, 'submit'])->name('exam.submit');
+    Route::get('exam/{id}/result', [studentExamController::class, 'examResult'])->name('exam.result');
+    Route::get('exam/{id}/merit-list', [studentExamController::class, 'meritList'])->name('exam.meritList');
+});
+
+Route::get('change-password', [dashboardController::class, 'showChangePasswordForm'])->middleware('auth')->name('changePassword');
+Route::post('change-password', [dashboardController::class, 'updatePassword'])->middleware('auth')->name('changePassword');
+Route::get('dashboard', function(){
+    $role = Auth::user()->role;
+    if($role == 'admin'){
+        return redirect()->route('admin.dashboard');
+    }
+    elseif($role == 'teacher'){
+        return redirect()->route('teacher.dashboard');
+    }
+    return redirect('/');
+})->middleware('auth')->name('dashboard');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/student/logout', 'Auth\LoginController@userLogout')->name('student.logout');      
-
-Route::prefix('admin')->group(function() {
-    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
-    Route::get('/', 'AdminController@index')->name('admin.dashboard'); 
-    Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
-    
-    //Password Reset Routes
-    Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');    
-    Route::get('/password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
-    Route::post('/password/reset', 'Auth\AdminResetPasswordController@reset');
-    Route::get('/password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');    
-});
-
-Route::prefix('lecturer')->group(function() {
-    Route::get('/login', 'Auth\LecturerLoginController@showLoginForm')->name('lecturer.login');
-    Route::post('/login', 'Auth\LecturerLoginController@login')->name('lecturer.login.submit');
-    Route::get('/', 'LecturerController@index')->name('lecturer.dashboard'); 
-    Route::get('/logout', 'Auth\LecturerLoginController@logout')->name('lecturer.logout');
-    
-    //Password Reset Routes
-    Route::post('/password/email', 'Auth\LecturerForgotPasswordController@sendResetLinkEmail')->name('lecturer.password.email');    
-    Route::get('/password/reset', 'Auth\LecturerForgotPasswordController@showLinkRequestForm')->name('lecturer.password.request');
-    Route::post('/password/reset', 'Auth\LecturerResetPasswordController@reset');
-    Route::get('/password/reset/{token}', 'Auth\LecturerResetPasswordController@showResetForm')->name('lecturer.password.reset');    
-});
